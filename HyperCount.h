eb6817702cf0dcf268609a5b93787a20666fc231
esalini22 <95315753+esalini22@users.noreply.github.com>
2023-04-10 17:33:30 -0400

    Update HyperCount.h

--- HyperCount.h
+++ HyperCount.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <immintrin.h> 
+//#include <unordered_map>
 typedef unsigned long long int ullint;
 using namespace std;
 class HyperCount{
@@ -10,17 +11,19 @@ class HyperCount{
         unsigned char p,b;
         unsigned int bits_v2; //bits para hacer & en operacion bitwise
         long double N; //corresponde a |M|: numero de buckets - se usa long double para evitar hacer casting al calcular cardinalidad
+        float a_m;
+        int ciclos_red;
         string sketch_size;
         vector<ullint> bit_mask; //vector de máscaras de bits, usadas para insertar registros
         //se usa vector de ullint, cada celda (64 bits) a su vez tiene hasta 12 buckets
         vector<ullint> sketch; //n sketches, n: numero de genomas
         vector<int> aux; //vector de indicadores auxiliares
         vector<int> freq; //vector de contadores de frecuencias
-        vector<ullint> demo; //vector de muestras demograficas
+        vector<unsigned short> demo; //vector de muestras demograficas (trazas ip)
         string name; //vector con los nombres de los genomas
         unsigned int min_val; //minimo valor en v2 para no sobrepasar registro 15
         //unordered_map<ullint,int> counter; //cuenta repeticiones de demo en sketch
-        unsigned int M; //contador de elementos insertados
+        unsigned M;
     public:
         HyperCount(unsigned char n1,unsigned char n2);
         ~HyperCount();
@@ -30,7 +33,7 @@ class HyperCount{
         void insert(ullint kmer); //inserta en sketch, para funciones hll y dist
         void loadSketch(char* infilename); //carga un archivo de sketch en memoria, para hll y dist
         void saveSketch(); //guarda el sketch individual en un archivo, para funcion sketch
-        //float estCard(); //cardinalidad individual
+        float estCard();
         vector<ullint> merge(HyperCount *hll); //hace la union y devuelve un nuevo sketch
         const vector<ullint> &getSketch();
         string getName();
